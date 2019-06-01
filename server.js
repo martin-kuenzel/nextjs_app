5447fed31f988c68d60d5f8ba7be45b93d4af1d1
mlkone <martin.kuenzel@hs-bochum.de>
2019-06-01 11:02:21 +0200

    server side simplifying urls in post example (page reload throws no 404 anymore)

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev});
+const handle = app.getRequestHandler();
+
+app.prepare().then(
+    () => {
+        const server = express();
+        const PORT = process.env.PORT || 3000;
+
+        server.get("/p/:id",(req,res) => {
+            const actualPage = "/post";
+            const queryParams = { title: req.params.id };
+            app.render( req, res, actualPage, queryParams );
+        });
+
+        server.get('*',(req,res)=>{
+            return handle(req,res);
+        });
+
+        server.listen( PORT, (err) => { 
+            if(err) throw err; 
+            console.log(`server running @${PORT}...`);
+        } );
+    }
+).catch(ex=>{
+    console.log(ex.stack);
+    process.exit(1);
+})
